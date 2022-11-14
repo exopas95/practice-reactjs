3a71afbff84aed5112d7e6a9114322b0499ef9b6
exopas95 <exopas58@gmail.com>
2022-11-14 19:20:41 +0900

    [ADD] Movie App

--- practice-coin-tracker/src/App.js
+++ practice-coin-tracker/src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 function App() {
     const [loading, setLoading] = useState(true);
     const [coins, setCoins] = useState([]);
+
     useEffect(() => {
         fetch("https://api.coinpaprika.com/v1/tickers")
             .then((response) => response.json())
@@ -11,6 +12,7 @@ function App() {
                 setLoading(false);
             });
     }, []);
+	
     return (
         <div>
             <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
